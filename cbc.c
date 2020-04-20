6632fa4a6a1a8ac1e801ee80b0e1c5eb6eab9f3e
sirshenmunsamy <63929876+sirshenmunsamy@users.noreply.github.com>
2020-04-20 14:11:21 +0200

    Add files via upload

--- cbc.c
+++ cbc.c
@@ -8,17 +8,26 @@ void XOR(char*, char*,char*, int);
 void CBC_encrypt(char*, char*, char*,const int, const int, const int, char*);
 void CBC_decrypt(char*, char*, char*,const int, const int, const int, char*);
 void padRight (char*, char*, int, int);
+
 int main()
 {
-	char * IV = ",./,./,./,./,./,./,./,./,./,./,.";
-	char * plaintext = "1234567890abcdefghijklmnopqrstuv";
-	char * key = "!@#$%^&*()!@#$%^&*()!@#$%^&*()!@";
+	char * UserIV = ",./,./,./,./,./,.";
+	char * Userplaintext = "1234567890123456";
+	char * Userkey = "!@#$%";
 
-	char ciphertext [32];
-	char newPlain [32];
+	char * plaintext [16];
+	char * key [32];
+	char * IV [16];
+
+	padRight (Userplaintext,plaintext,strlen(Userplaintext), 16);
+	padRight (Userkey,key,strlen(Userkey), 32);
+	padRight(UserIV, IV, strlen(UserIV), 16);
+
+	char ciphertext [16];
+	char newPlain [16];
     
 
-	CBC_encrypt(plaintext,IV,key, 32, 32, 128, ciphertext);
+	CBC_encrypt(plaintext,IV,key, 16, 16, 128, ciphertext);
 	//
 	// for (int i = 0; i < 32; i++)
  //    {
@@ -27,11 +36,11 @@ int main()
 
     printf("%s\n", " ");
 
-    //CBC_decrypt(ciphertext,IV,key, 32, 32, 128, newPlain);
+    CBC_decrypt(ciphertext,IV,key, 16, 16, 128, newPlain);
 
-    for (int i = 0; i < 32; i++)
+    for (int i = 0; i < 16; i++)
     {
-    	printf("%c", ciphertext[i]);
+    	printf("%c", newPlain[i]);
     }
 
 }
@@ -46,14 +55,14 @@ void XOR(char* str1, char* str2, char* str3, int size)
 
 void padRight (char* src, char* dest, int srcSize, int destSize) {
     int i = 0;
-    for (; i < srcSize; dest[i] = src[i++]);
+    for (; i < srcSize; dest[i++] = src[i]);
     for (; i < destSize; dest[i++] = 0); // pad with zeros to the right
 }
 
 void CBC_encrypt(char* plaintext, char* IV, char* key, const int curKeySize, const int inputSize, const int numBits, char * ciphertext)
 {
 	const int fullKeySize = 32;
-	unsigned char fullKey[fullKeySize];
+	char fullKey[fullKeySize];
 	unsigned char expandedKey[15][4][4];
 
 	//pad whatever needs to be padded
@@ -67,7 +76,7 @@ void CBC_encrypt(char* plaintext, char* IV, char* key, const int curKeySize, con
 	keyExpander(fullKey, expandedKey, numBits);
 	applyEncryptionRounds(first, expandedKey, numBits);
 
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < 16; i++)
     {
     	ciphertext[i] = first[i];
     	printf("%c", first[i]);
@@ -75,16 +84,6 @@ void CBC_encrypt(char* plaintext, char* IV, char* key, const int curKeySize, con
 	//ciphertext = first;
 	printf("%s\n", " ");
 
-	//keyExpander(fullKey, expandedKey, numBits);
-	applyEncryptionRounds(ciphertext, expandedKey, numBits);
-	char second [inputSize];
-	XOR(ciphertext, IV, second, inputSize);
-
-	for (int i = 0; i < 32; i++)
-    {
-    	//newPlain[i] = first[i];
-    	printf("%c", second[i]);
-    }
 
 }
 
@@ -97,13 +96,13 @@ void CBC_decrypt(char* ciphertext, char* IV, char* key, const int curKeySize, co
 	//pad whatever needs to be padded
 
 	keyExpander(fullKey, expandedKey, numBits);
-	applyEncryptionRounds(ciphertext, expandedKey, numBits);
+	applyDecryptionRounds(ciphertext, expandedKey, numBits);
 
 
 	char first [inputSize];
 	XOR(ciphertext, IV, first, inputSize);
 
-	for (int i = 0; i < 32; i++)
+	for (int i = 0; i < 16; i++)
     {
     	newPlain[i] = first[i];
     	//printf("%c", newPlain[i]);

0928c6ffe1b7bfe9d8135acb2bfc2d240fc023e3
sirshenmunsamy <63929876+sirshenmunsamy@users.noreply.github.com>
2020-04-20 16:30:59 +0200

    Add files via upload

--- cbc.c
+++ cbc.c
@@ -3,41 +3,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include "aes.c"
-
-void XOR(char*, char*,char*, int);
-void CBC_encrypt(char*, char*, char*,const int, const int, const int, char*);
-void CBC_decrypt(char*, char*, char*,const int, const int, const int, char*);
-void padRight (char*, char*, int, int);
+#include "cbc.h"
 
 int main()
 {
-	char * UserIV = ",./,./,./,./,./,.";
-	char * Userplaintext = "1234567890123456";
+	char * UserIV = "1234567890123456";
+	char * Userplaintext = "YoMaSePoesNaai12";
 	char * Userkey = "!@#$%";
 
-	char * plaintext [16];
-	char * key [32];
-	char * IV [16];
-
-	padRight (Userplaintext,plaintext,strlen(Userplaintext), 16);
-	padRight (Userkey,key,strlen(Userkey), 32);
-	padRight(UserIV, IV, strlen(UserIV), 16);
+	
 
 	char ciphertext [16];
 	char newPlain [16];
     
 
-	CBC_encrypt(plaintext,IV,key, 16, 16, 128, ciphertext);
-	//
-	// for (int i = 0; i < 32; i++)
- //    {
- //    	printf("%c", ciphertext[i]);
- //    }   
+	CBC_encrypt(Userplaintext,UserIV,Userkey, 16, 16, 128, ciphertext);
 
-    printf("%s\n", " ");
+    CBC_decrypt(ciphertext,UserIV,Userkey, 16, 16, 128, newPlain);
 
-    CBC_decrypt(ciphertext,IV,key, 16, 16, 128, newPlain);
-
+    for (int i = 0; i < 16; i++)
+    {
+    	printf("%c", ciphertext[i]);
+    }
+    printf("%s\n", "");
     for (int i = 0; i < 16; i++)
     {
     	printf("%c", newPlain[i]);
@@ -51,7 +39,7 @@ void XOR(char* str1, char* str2, char* str3, int size)
 	{
 		str3[i] = (char)((int)str1[i] ^ (int)str2[i]);
 	}
-}
+} 
 
 void padRight (char* src, char* dest, int srcSize, int destSize) {
     int i = 0;
@@ -59,10 +47,18 @@ void padRight (char* src, char* dest, int srcSize, int destSize) {
     for (; i < destSize; dest[i++] = 0); // pad with zeros to the right
 }
 
-void CBC_encrypt(char* plaintext, char* IV, char* key, const int curKeySize, const int inputSize, const int numBits, char * ciphertext)
+void CBC_encrypt(char* Userplaintext, char* UserIV, char* Userkey, const int curKeySize, const int inputSize, const int numBits, char * ciphertext)
 {
-	const int fullKeySize = 32;
-	char fullKey[fullKeySize];
+	//const int fullKeySize = 32;
+	//char fullKey[fullKeySize];
+	char * plaintext [16];
+	char * key [32];
+	char * IV [16];
+
+	padRight (Userplaintext,plaintext,strlen(Userplaintext), 16);
+	padRight (Userkey,key,strlen(Userkey), 32);
+	padRight(UserIV, IV, strlen(UserIV), 16);
+
 	unsigned char expandedKey[15][4][4];
 
 	//pad whatever needs to be padded
@@ -73,7 +69,7 @@ void CBC_encrypt(char* plaintext, char* IV, char* key, const int curKeySize, con
 	XOR(plaintext, IV, first, inputSize);
 
 	//printf("%s\n", " ");
-	keyExpander(fullKey, expandedKey, numBits);
+	keyExpander(key, expandedKey, numBits);
 	applyEncryptionRounds(first, expandedKey, numBits);
 
 	for (int i = 0; i < 16; i++)
@@ -87,15 +83,21 @@ void CBC_encrypt(char* plaintext, char* IV, char* key, const int curKeySize, con
 
 }
 
-void CBC_decrypt(char* ciphertext, char* IV, char* key, const int curKeySize, const int inputSize, const int numBits, char* newPlain)
+void CBC_decrypt(char* ciphertext, char* UserIV, char* Userkey, const int curKeySize, const int inputSize, const int numBits, char* newPlain)
 {
-	const int fullKeySize = 32;
-	unsigned char fullKey[fullKeySize];
+	//const int fullKeySize = 32;
+	//unsigned char fullKey[fullKeySize];
+	char * key [32];
+	char * IV [16];
+
+	padRight (Userkey,key,strlen(Userkey), 32);
+	padRight(UserIV, IV, strlen(UserIV), 16);
+
 	unsigned char expandedKey[15][4][4];
 
 	//pad whatever needs to be padded
 
-	keyExpander(fullKey, expandedKey, numBits);
+	keyExpander(key, expandedKey, numBits);
 	applyDecryptionRounds(ciphertext, expandedKey, numBits);
 
 
---
+++ cbc.h
@@ -0,0 +1,11 @@
+#ifndef CBC_H
+#define CBC_H
+
+int main();
+void XOR(char*, char*,char*, int);
+void CBC_encrypt(char*, char*, char*,const int, const int, const int, char*);
+void CBC_decrypt(char*, char*, char*,const int, const int, const int, char*);
+void padRight (char*, char*, int, int);
+
+
+#endif // CBC_H

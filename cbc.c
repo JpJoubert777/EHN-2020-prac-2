5e8951caad547c7c9fc969801c2833edb0619627
sirshenmunsamy <63929876+sirshenmunsamy@users.noreply.github.com>
2020-04-22 18:38:28 +0200

    Add files via upload

--- cbc.c
+++ cbc.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,15 +10,15 @@
 // int main()
 // {
 // 	char * UserIV = "1234567890123456";
-// 	char * Userplaintext = "EnjoyThisEHN";
+// 	char * Userplaintext = "EnjoyThisEHN12";
 // 	char * Userkey = "!@#$%";
 
 // 	char ciphertext [16];
 // 	char newPlain [16];
     
-// 	CBC_encrypt(Userplaintext,UserIV,Userkey, 16, 16, 128, ciphertext);
+// 	CBC_encrypt(Userplaintext,UserIV,Userkey, 16, 16, 128, 16, ciphertext);
 
-//     CBC_decrypt(ciphertext,UserIV,Userkey, 16, 16, 128, newPlain);
+//     CBC_decrypt(ciphertext,UserIV,Userkey, 16, 16, 128, 16, newPlain);
 
 //     for (int i = 0; i < 16; i++)
 //     {
@@ -59,15 +60,15 @@ void XOR(char* str1, char* str2, char* str3, int size)
  * @param[in]  numBits        The number bits
  * @param      ciphertext     The final ciphertext storing output
  */
-void CBC_encrypt(char* Userplaintext, char* UserIV, char* Userkey, const int curKeySize, const int inputSize, const int numBits, char * ciphertext)
+void CBC_encrypt(char* Userplaintext, char* UserIV, char* Userkey, const int curKeySize, const int inputSize, const int numBits,const int blocksize, char * ciphertext)
 {
-	char * plaintext [16]; //the size per block (16 bytes), will be padded if neccessary
+	char * plaintext [blocksize]; //the size per block (16 bytes), will be padded if neccessary
 	char * key [32]; //the max key size (32 bytes), also padded if neccessary
-	char * IV [16];//the max IV size (16 bytes), will be padded if neccessary
+	char * IV [blocksize];//the max IV size (16 bytes), will be padded if neccessary
 
-	padRight (Userplaintext,plaintext,strlen(Userplaintext), 16);
+	padRight (Userplaintext,plaintext,strlen(Userplaintext), blocksize);
 	padRight (Userkey,key,strlen(Userkey), 32);
-	padRight(UserIV, IV, strlen(UserIV), 16);
+	padRight(UserIV, IV, strlen(UserIV), blocksize);
 
 	unsigned char expandedKey[15][4][4];// to be passed to AES algorithm
 
@@ -77,7 +78,7 @@ void CBC_encrypt(char* Userplaintext, char* UserIV, char* Userkey, const int cur
 	keyExpander(key, expandedKey, numBits);
 	applyEncryptionRounds(first, expandedKey, numBits);//calls AES encryption alogorithm
 
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < blocksize; i++)
     {
     	ciphertext[i] = first[i];//Strore output of alogorithm in cihpertext
     }
@@ -98,13 +99,13 @@ void CBC_encrypt(char* Userplaintext, char* UserIV, char* Userkey, const int cur
  * @param[in]  numBits     The number of bits
  * @param      newPlain    The final plaintext storing output
  */
-void CBC_decrypt(char* ciphertext, char* UserIV, char* Userkey, const int curKeySize, const int inputSize, const int numBits, char* newPlain)
+void CBC_decrypt(char* ciphertext, char* UserIV, char* Userkey, const int curKeySize, const int inputSize, const int numBits, const int blocksize, char* newPlain)
 {
 	char * key [32];//The key to be used
-	char * IV [16];//the initialisation vector
+	char * IV [blocksize];//the initialisation vector
 
 	padRight (Userkey,key,strlen(Userkey), 32);//padding for correct size
-	padRight(UserIV, IV, strlen(UserIV), 16);//padding for correct size
+	padRight(UserIV, IV, strlen(UserIV), blocksize);//padding for correct size
 
 	unsigned char expandedKey[15][4][4];// to be passed to AES algorithm
 	keyExpander(key, expandedKey, numBits);
@@ -114,8 +115,9 @@ void CBC_decrypt(char* ciphertext, char* UserIV, char* Userkey, const int curKey
 	char first [inputSize];//stores output of XOR alogrithm
 	XOR(ciphertext, IV, first, inputSize);
 
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < blocksize; i++)
     {
     	newPlain[i] = first[i];//store output of algorithm in newPlain 
     }
 }
+
--- cbc.h
+++ cbc.h
@@ -3,8 +3,8 @@
 
 //int main();
 void XOR(char*, char*,char*, int);
-void CBC_encrypt(char*, char*, char*,const int, const int, const int, char*);
-void CBC_decrypt(char*, char*, char*,const int, const int, const int, char*);
+void CBC_encrypt(char*, char*, char*,const int, const int, const int, const int, char*);
+void CBC_decrypt(char*, char*, char*,const int, const int, const int, const int, char*);
 //void padRight (char*, char*, int, int);
 
 
